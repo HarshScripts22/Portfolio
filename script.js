caedf05ebcd93882a918b26ebaceefe99846b78d
Harsh Deshmukh <deshmukhharsh2507@gmail.com>
2024-10-16 21:02:58 +0530

    bug fixed

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+let icon = document.querySelector(".mode");
+
+icon.addEventListener("click", (e) => {
+  document.body.classList.toggle("dark-theme");
+
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "./sun.png";
+  } else {
+    icon.src = "./moon.png";
+  }
+});
